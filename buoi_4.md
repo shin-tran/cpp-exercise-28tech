96ce21ff58342bb40b7f71923f5e591b9a3640c5
ngocshintrannn <ngoc.shintrann@gmail.com>
2024-10-02 21:39:38 +0700

    upload bai 6, 7

--- buoi_4.md
+++ buoi_4.md
@@ -211,11 +211,89 @@ int main() {
 #### Bài 6
 
 ```c
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  int i = 2;
+  while (i <= n) {
+    if (n % i == 0) {
+      cout << i << " ";
+      n /= i;
+    } else {
+      i++;
+    }
+  }
+  return 0;
+}
 ```
 
 #### Bài 7
 
 ```c
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+bool laSoThuanNghich(int n) {
+  int temp = n;
+  int soDao = 0;
+  while (temp > 0) {
+    soDao = soDao * 10 + temp % 10;
+    temp /= 10;
+  }
+  return n == soDao;
+}
+
+int demUocSoNguyenTo(int n) {
+  int uocSo[100] = {0}; // Giả sử số lượng ước số nguyên tố không quá 100
+  int soUoc = 0;
+  for (int i = 2; i <= sqrt(n); i++) {
+    while (n % i == 0) {
+      bool daTonTai = false;
+      for (int j = 0; j < soUoc; j++) {
+        if (uocSo[j] == i) {
+          daTonTai = true;
+          break;
+        }
+      }
+      if (!daTonTai) {
+        uocSo[soUoc++] = i;
+      }
+      n /= i;
+    }
+  }
+  if (n > 1) {
+    bool daTonTai = false;
+    for (int j = 0; j < soUoc; j++) {
+      if (uocSo[j] == n) {
+        daTonTai = true;
+        break;
+      }
+    }
+    if (!daTonTai) {
+      uocSo[soUoc++] = n;
+    }
+  }
+  return soUoc;
+}
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+
+  for (int i = a; i <= b; i++) {
+    if (laSoThuanNghich(i) && demUocSoNguyenTo(i) >= 3) {
+      cout << i << " ";
+    }
+  }
+
+  return 0;
+}
 ```
 
 #### Bài 8
--- compiler/main.cpp
+++ compiler/main.cpp
@@ -1,25 +1,60 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-int main() {
-  int start;
-  cin >> start;
-  if (start < 2 || start > 92) {
-    cout << "Dau vao khong hop le.";
-    return 1;
+bool laSoThuanNghich(int n) {
+  int temp = n;
+  int soDao = 0;
+  while (temp > 0) {
+    soDao = soDao * 10 + temp % 10;
+    temp /= 10;
   }
-  long long a = 0, b = 1; 
+  return n == soDao;
+}
 
-  for (int i = 0; i < start; i++) {
-    if (i <= 1) {
-      cout << i << " "; 
-    } else {
-      long long c = a + b;
-      cout << c << " ";
-      a = b;
-      b = c;
+int demUocSoNguyenTo(int n) {
+  int uocSo[100] = {0}; // Giả sử số lượng ước số nguyên tố không quá 100
+  int soUoc = 0;
+  for (int i = 2; i <= sqrt(n); i++) {
+    while (n % i == 0) {
+      bool daTonTai = false;
+      for (int j = 0; j < soUoc; j++) {
+        if (uocSo[j] == i) {
+          daTonTai = true;
+          break;
+        }
+      }
+      if (!daTonTai) {
+        uocSo[soUoc++] = i;
+      }
+      n /= i;
     }
   }
+  if (n > 1) {
+    bool daTonTai = false;
+    for (int j = 0; j < soUoc; j++) {
+      if (uocSo[j] == n) {
+        daTonTai = true;
+        break;
+      }
+    }
+    if (!daTonTai) {
+      uocSo[soUoc++] = n;
+    }
+  }
+  return soUoc;
+}
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+
+  for (int i = a; i <= b; i++) {
+    if (laSoThuanNghich(i) && demUocSoNguyenTo(i) >= 3) {
+      cout << i << " ";
+    }
+  }
+
   return 0;
 }
